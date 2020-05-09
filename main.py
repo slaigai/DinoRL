bd8cc351fed12701b7abd38f0f430f301872b29f
Steven Lai <laisteve17@gmail.com>
2020-05-09 12:42:35 -0700

    Update dino template, set browser size to minimum, optimize region for dino detection

--- main.py
+++ main.py
@@ -2,21 +2,11 @@
 dino_url = 'chrome://dino'
 
 
-def find_template(img, template):
-	h, w, _ = template.shape
-
-	res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF)
-	min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-	top_left = max_loc
-	bottom_right = (top_left[0] + w, top_left[1] + h)
-	return (top_left, bottom_right)
-
-
 def init_driver():
 	# Search for chromedriver in current directory. Ignored by gitignore because each environment may have a different chrome driver.
 	driver = webdriver.Chrome('./chromedriver')
 	driver.set_window_position(0, 0)
-	driver.set_window_size(500, 500)
+	driver.set_window_size(500, 375) # minimum size for chrome window
 	driver.get(dino_url)
 	return driver
 
@@ -27,10 +17,6 @@ def screenshot_elem(elem):
 	img = cv2.cvtColor(numpy.array(img), cv2.COLOR_RGBA2BGR)
 	return img
 
-
-def mark_template(img, template):
-	top_left, bottom_right = find_template(img, template)
-	cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 2)
 
 
 if __name__ == "__main__":
@@ -79,27 +65,9 @@ if __name__ == "__main__":
 		# is_game_over
 		#     Runner.instance_.crashed
 
-		watch({"top": 0, "left": 0, "width": 500, "height": 600})
-		# while "Screen capturing":
-		# 	# Mark template with green box
-		# 	img = screenshot_elem(body)
-		# 	# mark_template(img, template)
-		# 	cv2.imshow("image", img)
-		#
-		# 	# Press "q" to quit
-		# 	if cv2.waitKey(25) & 0xFF == ord("q"):
-		# 		cv2.destroyAllWindows()
-		# 		print()
-		# 		break
+		watch({"top": 0, "left": 0, "width": 500, "height": 375})
 
-	# im = Image.open(BytesIO(png))
-	# im.show()
 	finally:
 		driver.quit()
-
-# elem = driver.find_element_by_tag_name('body')
-# elem.send_keys(Keys.SPACE)
-
-
 
 
--- watch.py
+++ watch.py
@@ -5,8 +5,34 @@ import mss
 import numpy
 from progressbar import progressbar
 
+
+def find_template(img, template):
+    h, w, _ = template.shape
+
+    res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    return top_left, bottom_right
+
+
+def mark_template(img, template):
+    top_left, bottom_right = find_template(img, template)
+    cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 2)
+
+
+def mark_dino(img, dino_template):
+    dino_region = {'left': 0, 'right': 150, 'top': 300, 'bottom': 670}
+    img_cropped = img[dino_region['top']:dino_region['bottom'] + 1, dino_region['left']:dino_region['right'] + 1, :]
+    top_left, bottom_right = find_template(img_cropped, dino_template)
+    top_left = (top_left[0] + dino_region['left'], top_left[1] + dino_region['top'])
+    bottom_right = (bottom_right[0] + dino_region['left'], bottom_right[1] + dino_region['top'])
+    cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 2)
+
+
 def watch(monitor):
     pb = progressbar(min=0, max=60, bar_size=25)
+    dino_template = cv2.imread('templates/dino.png')
     with mss.mss() as sct:
         # Part of the screen to capture
         cv2.namedWindow('image',cv2.WINDOW_NORMAL)
@@ -16,7 +42,10 @@ def watch(monitor):
             last_time = time.time()
 
             # Get raw pixels from the screen, save it to a Numpy array
-            img = numpy.array(sct.grab(monitor))
+            frame = numpy.array(sct.grab(monitor))
+            img = cv2.cvtColor(frame, cv2.COLOR_RGBA2BGR)
+            mark_dino(img, dino_template)
+            # mark_template(img, dino_template)
 
             # Display the picture
             cv2.imshow("image", img)
@@ -26,7 +55,7 @@ def watch(monitor):
             pb.print_progress(fps)
 
             # Press "q" to quit
-            if cv2.waitKey(25) & 0xFF == ord("q"):
+            if cv2.waitKey(5) & 0xFF == ord("q"):
                 cv2.destroyAllWindows()
                 print()
                 break
